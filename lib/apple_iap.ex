c60f646c8f6135dddeb3af66aa4cd4139520c5f6
Blain Smith <rebelgeek@blainsmith.com>
2017-04-03 09:48:02 -0400

    Receipt or error response

--- lib/apple_iap.ex
+++ lib/apple_iap.ex
@@ -5,21 +5,18 @@ defmodule AppleIAP do
 
   alias AppleIAP.Client
 
-  @doc """
-  Updates a previous transaction with a new billing cycle date
-
-  Args:
-    * `receipt_data` - The base64 encoded receipt data
-    * `password`- Your app's shared secret
-  """
-  @spec verify_receipt(atom, String.t, String.t) :: HTTPoison.Response
-  def verify_receipt(:sandbox, password, receipt_data) do
-    body = %{
-        "receipt-data": receipt_data,
-        "password": password
-      }
-
-    Client.post("https://sandbox.itunes.apple.com/verifyReceipt", body, [{"content-type", "application/json"}])
+  defmodule Receipt do
+    defstruct(quantity: nil,
+              product_id: nil,
+              transaction_id: nil,
+              original_transaction_id: nil,
+              purchase_date: nil,
+              original_purchase_date: nil,
+              expires_date: nil,
+              cancellation_date: nil,
+              app_item_id: nil,
+              version_external_identifier: nil,
+              web_order_line_item_id: nil)
   end
 
   @doc """
@@ -29,13 +26,64 @@ defmodule AppleIAP do
     * `receipt_data` - The base64 encoded receipt data
     * `password`- Your app's shared secret
   """
-  @spec verify_receipt(String.t, String.t) :: HTTPoison.Response
+  @spec verify_receipt(atom, String.t, String.t) :: {:ok, Receipt.t | :error, String.t}
+  def verify_receipt(:sandbox, password, receipt_data) do
+    body = %{
+        "receipt-data": receipt_data,
+        "password": password
+      }
+
+    case do_post("https://sandbox.itunes.apple.com/verifyReceipt", body) do
+      {:ok, response} -> to_receipt(response.body)
+      {:error, error} -> {:error, error.reason}
+    end
+  end
+
+  @doc """
+  Updates a previous transaction with a new billing cycle date
+
+  Args:
+    * `receipt_data` - The base64 encoded receipt data
+    * `password`- Your app's shared secret
+  """
+  @spec verify_receipt(String.t, String.t) :: {:ok, Receipt.t | :error, String.t}
   def verify_receipt(password, receipt_data) do
     body = %{
         "receipt-data": receipt_data,
         "password": password
       }
 
-    Client.post("https://buy.itunes.apple.com/verifyReceipt", body, [{"content-type", "application/json"}])
+    case do_post("https://buy.itunes.apple.com/verifyReceipt", body) do
+      {:ok, response} -> to_receipt(response.body)
+      {:error, error} -> {:error, error.reason}
+    end
+  end
+
+  defp do_post(url, body) do
+    Client.post(url, body, [{"content-type", "application/json"}])
+  end
+
+  defp to_receipt(body) do
+    case body["status"] do
+      21000 -> {:error, "The App Store could not read the JSON object you provided."}
+      21002 -> {:error, "The data in the receipt-data property was malformed or missing."}
+      21003 -> {:error, "The receipt could not be authenticated."}
+      21005 -> {:error, "The receipt server is not currently available."}
+      21007 -> {:error, "This receipt is from the test environment, but it was sent to the production environment for verification. Send it to the test environment instead."}
+      21008 -> {:error, "This receipt is from the production environment, but it was sent to the test environment for verification. Send it to the production environment instead."}
+      _valid -> {:ok, %Receipt{
+                        quantity: body["receipt"]["quantity"],
+                        product_id: body["receipt"]["product_id"],
+                        transaction_id: body["receipt"]["transaction_id"],
+                        original_transaction_id: body["receipt"]["original_transaction_id"],
+                        purchase_date: body["receipt"]["purchase_date"],
+                        original_purchase_date: body["receipt"]["original_purchase_date"],
+                        expires_date: body["receipt"]["expires_date"],
+                        cancellation_date: body["receipt"]["cancellation_date"],
+                        app_item_id: body["receipt"]["app_item_id"],
+                        version_external_identifier: body["receipt"]["version_external_identifier"],
+                        web_order_line_item_id: body["receipt"]["web_order_line_item_id"]
+                      }}
+    end
   end
 end
--- test/apple_iap_test.exs
+++ test/apple_iap_test.exs
@@ -11,28 +11,28 @@ defmodule AppleIAPTest do
   end
 
   test "verify_receipt/2" do
+    receipt = %AppleIAP.Receipt{
+      quantity: 1,
+      product_id: "com.oddnetworks",
+      transaction_id: "t_1",
+      original_transaction_id: "ot_1",
+      purchase_date: "2012-07-22T14:59:50",
+      original_purchase_date: "2012-07-22T14:59:50",
+      expires_date: "2012-07-22T14:59:50",
+      cancellation_date: "2012-07-22T14:59:50",
+      app_item_id: "a_1",
+      version_external_identifier: "e_1",
+      web_order_line_item_id: "w_o_1"
+    }
+
     request = %{
       "receipt-data" => "BA5E64",
       "password" => "pass1"
     }
 
     response = %{
-      "status" => "t_2",
+      "status" => 0,
       "receipt" => %{
-        "quantity" => 1,
-        "product_id" => "com.oddnetworks",
-        "transaction_id" => "t_1",
-        "original_transaction_id" => "ot_1",
-        "purchase_date" => "2012-07-22T14:59:50",
-        "original_purchase_date" => "2012-07-22T14:59:50",
-        "expires_date" => "2012-07-22T14:59:50",
-        "cancellation_date" => "2012-07-22T14:59:50",
-        "app_item_id" => "a_1",
-        "version_external_identifier" => "e_1",
-        "web_order_line_item_id" => "w_o_1"
-      },
-      "latest_receipt" => "BA5E64",
-      "latest_receipt_info" => %{
         "quantity" => 1,
         "product_id" => "com.oddnetworks",
         "transaction_id" => "t_1",
@@ -58,17 +58,26 @@ defmodule AppleIAPTest do
       ])
     expect(:hackney, :body, 1, {:ok, Poison.encode!(response)})
 
-    assert AppleIAP.verify_receipt("pass1", "BA5E64") ==
-      {:ok, %HTTPoison.Response{
-          status_code: 200,
-          body: response
-        }
-      }
+    assert {:ok, receipt} == AppleIAP.verify_receipt("pass1", "BA5E64")
 
     assert validate :hackney
   end
 
   test "verify_receipt/3" do
+    receipt = %AppleIAP.Receipt{
+      quantity: 1,
+      product_id: "com.oddnetworks",
+      transaction_id: "t_1",
+      original_transaction_id: "ot_1",
+      purchase_date: "2012-07-22T14:59:50",
+      original_purchase_date: "2012-07-22T14:59:50",
+      expires_date: "2012-07-22T14:59:50",
+      cancellation_date: "2012-07-22T14:59:50",
+      app_item_id: "a_1",
+      version_external_identifier: "e_1",
+      web_order_line_item_id: "w_o_1"
+    }
+
     request = %{
       "receipt-data" => "BA5E64",
       "password" => "pass1"
@@ -77,20 +86,6 @@ defmodule AppleIAPTest do
     response = %{
       "status" => "t_2",
       "receipt" => %{
-        "quantity" => 1,
-        "product_id" => "com.oddnetworks",
-        "transaction_id" => "t_1",
-        "original_transaction_id" => "ot_1",
-        "purchase_date" => "2012-07-22T14:59:50",
-        "original_purchase_date" => "2012-07-22T14:59:50",
-        "expires_date" => "2012-07-22T14:59:50",
-        "cancellation_date" => "2012-07-22T14:59:50",
-        "app_item_id" => "a_1",
-        "version_external_identifier" => "e_1",
-        "web_order_line_item_id" => "w_o_1"
-      },
-      "latest_receipt" => "BA5E64",
-      "latest_receipt_info" => %{
         "quantity" => 1,
         "product_id" => "com.oddnetworks",
         "transaction_id" => "t_1",
@@ -116,12 +111,34 @@ defmodule AppleIAPTest do
       ])
     expect(:hackney, :body, 1, {:ok, Poison.encode!(response)})
 
-    assert AppleIAP.verify_receipt(:sandbox, "pass1", "BA5E64") ==
-      {:ok, %HTTPoison.Response{
-          status_code: 200,
-          body: response
+    assert {:ok, receipt} == AppleIAP.verify_receipt(:sandbox, "pass1", "BA5E64")
+
+    assert validate :hackney
+  end
+
+  test "error is returned from apple" do
+    request = %{
+      "receipt-data" => "BA5E64",
+      "password" => "pass1"
+    }
+
+    response = %{
+      "status" => 21003,
+      "receipt" => %{}
+    }
+
+    expect(
+      :hackney, 
+      :request, 
+      [
+        {
+          [:post, "https://buy.itunes.apple.com/verifyReceipt", [{"accept", "application/json"}, {"content-type", "application/json"}], Poison.encode!(request), []],
+          {:ok, 200, [], :client}
         }
-      }
+      ])
+    expect(:hackney, :body, 1, {:ok, Poison.encode!(response)})
+
+    assert {:error, "The receipt could not be authenticated."} == AppleIAP.verify_receipt("pass1", "BA5E64")
 
     assert validate :hackney
   end
